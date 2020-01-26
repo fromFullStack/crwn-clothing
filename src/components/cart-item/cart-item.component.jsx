913d4dbfc1860a4d487c0564bdd2097c0f54ba82
Denis P <29834392+DenisP01@users.noreply.github.com>
2020-01-26 20:44:51 +0500

    Updating application with reselect? replaced cart selectors with memolized selectors

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,12 +5,12 @@ import "./cart-item.styles.scss";
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="item" />
-    <dev className="item-details">
+    <div className="item-details">
       <span className="name">{name}</span>
       <span className="price">
         {quantity} x ${price}
       </span>
-    </dev>
+    </div>
   </div>
 );
 
